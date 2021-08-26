11837d1bba213245683b4275ff6c18c7f5018403
Christos Kontas <xakon@yahoo.com>
2021-08-26 14:45:57 +0200

    panics: solutions

--- about_panics.go
+++ about_panics.go
@@ -1,13 +1,19 @@
 package go_koans
 
-func divideFourBy(i int) int {
+func divideFourBy(i int) (n int) {
+	defer func() {
+		if recover() != nil {
+			n = 2
+		}
+	}()
+
 	return 4 / i
 }
 
 const __divisor__ = 0
 
 func aboutPanics() {
-	assert(__delete_me__) // panics are exceptional errors at runtime
+	assert(true) // panics are exceptional errors at runtime
 
 	n := divideFourBy(__divisor__)
 	assert(n == 2) // panics are exceptional errors at runtime
